ac54d8c355e772005d104dbee72e89b22db90c24
Alex Patterson <alex@codingcat.dev>
2022-07-08 13:43:27 -0400

    yt

--- src/youtube.js
+++ src/youtube.js
@@ -17,23 +17,18 @@ const youtube = google.youtube({
 });
 
 // Save this if you need a new token
-// async function getTokenCode() {
-//   const oauth2Client = new google.auth.OAuth2(
-//     "67840007708-ueorlqaa4h7pg397vf4q3a7l6ilk3s1t.apps.googleusercontent.com",
-//     "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",
-//     "http://localhost:9797/oauth2callback"
-//   );
-//   const scopes = [
-//     "profile",
-//     "email",
-//     "https://www.googleapis.com/auth/youtube", // <-- sensitive scope
-//   ];
-//   const url = oauth2Client.generateAuthUrl({
-//     access_type: "offline",
-//     scope: scopes,
-//   });
-// console.log(url);
-// }
+async function getTokenCode() {
+  const scopes = [
+    "profile",
+    "email",
+    "https://www.googleapis.com/auth/youtube", // <-- sensitive scope
+  ];
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+  });
+console.log(url);
+}
 
 async function getBroadcasts() {
   const broadcastsRes = await youtube.liveBroadcasts.list({
